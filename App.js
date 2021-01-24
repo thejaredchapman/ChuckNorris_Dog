165dec6f228c4aaa65d3220f3ea4eba43f53721c
Jared Chapman <thejaredchapman@gmail.com>
2021-01-24 00:41:57 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Sign';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkform/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+const app = new Clarifai.App({
+    apiKey: 'f317ccaf2b3343938112179a18293946'
+});
+
+const particlesOptions = {
+  particles : {
+    number: {
+      value: 30,
+      density: {
+        enable: true, 
+        value_area: 800
+      }
+    }
+  }
+}
+
+class App extends Component {
+    constructor() {
+      super();
+      this.state = {
+        input: '',
+        imageUrl:'',
+        box: {},
+        route:'signin'
+      }
+    }
+
+    calculateFaceLocation = (data) => {
+      const clarifaiFace = outputs[0].data.regions[0].region_info.bounding_box;
+      const image = document.getElementById('inputimage');
+      const width = Number(image.width);
+      const height = Number(image.height);
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row) * height)
+      }
+    }
+
+    displaceFaceBox = (box) => {
+      console.log(box);
+      this.setState({box: box});
+    }
+
+    onInputChange = (event) => {
+      console.log({input: event.target.value});
+    }
+    
+    onButtonSubmit = () => {
+      this.setState({imageUrl:this.state.input});
+      app.models.predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+        .then(response => this.displaceFaceBox(this.calculateFaceLocation(response))
+        .catch(err) => console.log(err));
+        }
+    }
+    
+    onRouteChange = (route) => {
+      this.setState({route: 'home'});
+    }
+
+    render() {
+      return (        
+        <div className="App">
+           <Particles className='particles'
+            params={particlesOptions}            
+           />
+          <Navigation onRouteChange={this.onRouteChange} />
+          { this.state.route === 'signin' 
+            ? <Signin onRouteChange={this.onRouteChange}/>
+            : <div> 
+                <Logo />
+                  <Rank />
+                  <ImageLinkForm 
+                    onInputChange={this.onInputChange} 
+                    onButtonSubmit={this.onButtonSubmit}/>
+                  <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+              </div>   
+        </div>
+    );
+  }
+}
